c0260ae5c8d5e1274271ec874748d7ae89b1e5b5
LTA <37591675+LikeToAccess@users.noreply.github.com>
2021-02-23 00:55:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from pynput.keyboard import Key
 from pynput import keyboard
+from os import system
 from time import sleep
+import invintory
 
 
 keyboard_controller = keyboard.Controller()
+invintory_list = ["⛏","⚔","🛡","$","&","1","1","1","1","2","3","4","5","6"]
+
+
+def clear():
+	system("clear")
+	
 def on_release(key):
 	if key:
-		print(key)
-
-keyboard_listener = keyboard.Listener(on_release=on_release)
-keyboard_listener.start()
+		return(key)
 
 
-while True:
-	sleep(1)
+if __name__ == "__main__":
+	keyboard_listener = keyboard.Listener(on_release=on_release)
+	keyboard_listener.start()
+	while True:
+		for i in range(36):
+			clear()
+			selected = i
+			invintory.menu((9,4), invintory.highlight_selected(invintory_list, selected))
+			sleep(0.3)
